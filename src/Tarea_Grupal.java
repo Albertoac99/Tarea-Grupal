01e845564a3ca3dc4d6121c4c3e30fb53a933e9a
DavidRM <eldaviddjrodmar@gmail.com>
2020-02-14 12:37:18 +0100

    Pruebas de la clase tarjeta
    Signed-off-by: DavidRM <eldaviddjrodmar@gmail.com>

---
+++ src/Tarea_Grupal.java
@@ -0,0 +1,10 @@
+
+public class Tarea_Grupal {
+
+	public static void main(String[] args) {
+		TarjetaDebito tarjeta1 = new Tarjeta("1","12345678P",1234,1000);
+		TarjetaMonedero tarjeta2 = new Tarjeta("2","12447678P",1111,2000);
+
+	}
+
+}
